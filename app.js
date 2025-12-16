37e48e335cf0b86f0230b9f0fa5d6662b874b3fc
AlderSan <gentr023@gmail.com>
2025-12-16 14:52:46 -0600

    Move routing logic out of main app.js into router

--- app.js
+++ app.js
@@ -1,64 +1,20 @@
 const express = require("express");
+const dotenv = require("dotenv").config();
 const app = express();
 const path = require("node:path");
-const PORT = 3000;
 const assetsPath = path.join(__dirname, "public");
 const bodyParser = require("body-parser");
+const PORT = process.env.SERVER_PORT;
 
-//starting messages
-const messages = [
-  {
-    text: "Hi there!",
-    user: "Amando",
-    added: new Date(),
-    id: 1
-  },
-  {
-    text: "Hello World!",
-    user: "Charles",
-    added: new Date(),
-    id: 2
-  },
-  {text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam ac blandit dolor. Maecenas sollicitudin malesuada leo. Nulla vel tellus enim. Maecenas pellentesque ante sem, eget.",
-    user: "Alex",
-    added: new Date(),
-    id: 3
-  }
-];
 
 //enable public assets and EJS engine
 app.use(express.static(assetsPath));
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
-//app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());
 
+app.use("/", messagesRouter);
 
-app.get("/", (req, res) => {
-  res.render("index", { messages: messages });
-  console.log('index render working');
-});
-
-app.get("/new", (req, res) => {
-  res.render("new");
-  console.log('new render working');
-});
-app.post("/new", (req, res) => {
-  const formData = req.body; // The parsed JSON data is in req.body
-  console.log('Received form data:', formData);
-  messages.push({ text: formData.text, user: formData.user, added: new Date(), id: (messages.length + 1) });
-  res.status(200).json({ message: 'Form data received successfully!', data: formData });
-});
-
-app.get("/open/:messageId", (req, res) => {
-  const { messageId } = req.params;
-  console.log(messageId);
-  res.render('openmessage', {message: messages[messageId - 1]});
-});
-
-app.get("/{*splat}", (req, res) => {
-  res.redirect("/");
-});
 
 app.listen(PORT, (error) => {
   // This is important!
---
+++ db/populatedb.js
@@ -0,0 +1,20 @@
+//starting messages
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+    id: 1
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date(),
+    id: 2
+  },
+  {text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam ac blandit dolor. Maecenas sollicitudin malesuada leo. Nulla vel tellus enim. Maecenas pellentesque ante sem, eget.",
+    user: "Alex",
+    added: new Date(),
+    id: 3
+  }
+];
